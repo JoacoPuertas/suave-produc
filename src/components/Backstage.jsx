c5cd3b6004f417886187bc34f96686fc2a769d90
Joaquin <joacopuertas3@gmail.com>
2024-09-12 13:05:06 -0300

    backstage responsive en ambos dispositivos

--- src/components/Backstage.jsx
+++ src/components/Backstage.jsx
@@ -11,6 +11,8 @@ import Suave4 from '../assets/Fotos/Suave4.jpg';
 import Suave5 from '../assets/Fotos/Suave5.jpg';
 import Suave6 from '../assets/Fotos/Suave6.jpg';
 import Suave7 from '../assets/Fotos/Suave7.jpg';
+import Suave8 from '../assets/Fotos/Suave8.jpg';
+import Suave9 from '../assets/Fotos/Suave9.jpg';
 
 function Backstage() {
   return (
@@ -18,14 +20,18 @@ function Backstage() {
       <h2 className='text-center mb-5'>Backstage</h2>
       
       {/* Primer fila en mobile */}
-      <Row> 
+      <Row className='d-flex'> 
         <Col xs={12} md={4} className="gallery-item gallery-item-large" style={{ backgroundImage: `url(${Suave0})` }}></Col>
 
         {/* Columna que contiene las dos imágenes pequeñas */}
-        <Col xs={12} md={4}>
+        <Col className='midCol' xs={12} md={4}>
           <Row className='midrow container-fluid'>
             <Col xs={6} className="gallery-item gallery-item-half" style={{ backgroundImage: `url(${Suave1})` }}></Col>
             <Col xs={6} className="gallery-item gallery-item-half" style={{ backgroundImage: `url(${Suave2})` }}></Col>
+          </Row>
+          <Row className='midrow container-fluid d-none d-md-flex'>
+            <Col xs={6} className="gallery-item gallery-item-half" style={{ backgroundImage: `url(${Suave8})` }}></Col>
+            <Col xs={6} className="gallery-item gallery-item-half" style={{ backgroundImage: `url(${Suave9})` }}></Col>
           </Row>
         </Col>
 
@@ -33,7 +39,7 @@ function Backstage() {
       </Row>
 
       {/* Segunda fila solo para pantallas grandes */}
-      <Row className="g-2 mt-3 d-none d-md-flex">
+      <Row className="d-none d-md-flex">
         <Col md={3} className="gallery-item" style={{ backgroundImage: `url(${Suave4})` }}></Col>
         <Col md={3} className="gallery-item" style={{ backgroundImage: `url(${Suave5})` }}></Col>
         <Col md={3} className="gallery-item" style={{ backgroundImage: `url(${Suave6})` }}></Col>
