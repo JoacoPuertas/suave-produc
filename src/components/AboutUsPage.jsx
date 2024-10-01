61b7c08b6f070150a2809b7781a51be215cb7f92
Joaquin <joacopuertas3@gmail.com>
2024-10-01 00:22:09 -0300

    Casi terminado neneeeee

--- src/components/AboutUsPage.jsx
+++ src/components/AboutUsPage.jsx
@@ -1,10 +1,44 @@
 import React from 'react';
+import { Card, Container, Row, Col } from 'react-bootstrap';
+import './aboutUsPage.css';
+import img1 from '../assets/Juli.png';
+import img2 from '../assets/Juli2.jpg';
 
 function AboutUs() {
   return (
-    <div>
-      <h1>About Us</h1>
-      <p>This is the About Us page.</p>
+    <div className="about-section">
+      {/* Fondo con texto */}
+      <div className="about-bg">
+        <div className="overlay">
+          <p className="about-text">
+          Suave es una productora artística que nace de la amistad y la colaboración. Creamos espacios únicos y memorables, desde la decoración de eventos hasta la creación de entornos especiales para artistas. En Suave, creemos en la autogestión y en la importancia de la suavidad en nuestras relaciones, aportando un toque de delicadeza y empatía a cada proyecto. Nuestro objetivo es construir experiencias armoniosas y auténticas que resalten la belleza de la conexión humana y la creatividad compartida.
+          </p>
+        </div>
+      </div>
+
+      {/* Sección con dos cards para el personal */}
+      <Container className="mt-5">
+        <Row className="justify-content-center">
+          <Col md={6} lg={4}>
+            <Card className="team-card">
+              <Card.Img variant="top" src={img1} />
+              <Card.Body className='py-1 px-0'>
+                <Card.Title className='py-1 px-3'>Julieta Puertas</Card.Title>
+                <Card.Text className='py-1 px-3'>Prouctora - Fotografa</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+          <Col md={6} lg={4}>
+            <Card className="team-card">
+              <Card.Img variant="top" src={img2} />
+              <Card.Body className='py-1 px-0'>
+                <Card.Title className='py-1 px-3'>Julieta Ventanas</Card.Title>
+                <Card.Text className='py-1 px-3'>Prouctora - Fotografa</Card.Text>
+              </Card.Body>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
--- src/components/BackstagePage.jsx
+++ src/components/BackstagePage.jsx
@@ -10,6 +10,18 @@ import img5 from '../assets/Fotos/Suave4.jpg';
 import img6 from '../assets/Fotos/Suave5.jpg';
 import img7 from '../assets/Fotos/Suave6.jpg';
 import img8 from '../assets/Fotos/Suave7.jpg';
+import img9 from '../assets/Fotos/Suave8.jpg';
+import img10 from '../assets/Fotos/Suave9.jpg';
+import img11 from '../assets/Fotos/Suave10.jpg';
+import img12 from '../assets/Fotos/Suave11.jpg';
+import img13 from '../assets/Fotos/Suave12.jpg';
+import img14 from '../assets/Fotos/Suave13.jpg';
+import img15 from '../assets/Fotos/Suave14.jpg';
+import img16 from '../assets/Fotos/Suave15.jpg';
+import img17 from '../assets/Fotos/Suave16.jpg';
+import img18 from '../assets/Fotos/Suave17.jpg';
+import img19 from '../assets/Fotos/Suave18.jpg';
+import img20 from '../assets/Fotos/Suave19.jpg';
 
 function BackstagePage() {
   return (
@@ -22,6 +34,18 @@ function BackstagePage() {
       <div className="grid-item"><img src={img6} alt="Imagen 6" /></div>
       <div className="grid-item"><img src={img7} alt="Imagen 7" /></div>
       <div className="grid-item"><img src={img8} alt="Imagen 8" /></div>
+      <div className="grid-item"><img src={img9} alt="Imagen 9" /></div>
+      <div className="grid-item"><img src={img10} alt="Imagen 10" /></div>
+      <div className="grid-item"><img src={img11} alt="Imagen 11" /></div>
+      <div className="grid-item"><img src={img12} alt="Imagen 12" /></div>
+      <div className="grid-item"><img src={img13} alt="Imagen 13" /></div>
+      <div className="grid-item"><img src={img14} alt="Imagen 14" /></div>
+      <div className="grid-item"><img src={img15} alt="Imagen 15" /></div>
+      <div className="grid-item"><img src={img16} alt="Imagen 16" /></div>
+      <div className="grid-item"><img src={img17} alt="Imagen 17" /></div>
+      <div className="grid-item"><img src={img18} alt="Imagen 18" /></div>
+      <div className="grid-item"><img src={img19} alt="Imagen 19" /></div>
+      <div className="grid-item"><img src={img20} alt="Imagen 20" /></div>
     </div>
   );
 }
